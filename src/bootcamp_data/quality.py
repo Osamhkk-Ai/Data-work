50d28f622e70a415db954ea5baf81a244237b1c0
Osamhkk-Ai <osamh.wk@gmail.com>
2025-12-23 00:22:34 +0300

    add 2 func in the quality.py file

--- src/bootcamp_data/quality.py
+++ src/bootcamp_data/quality.py
@@ -1,7 +1,7 @@
-# Chick if there is col not in the dataFrame
+# Check if there is col not in the dataFrame
 def require_columns(df, cols):
-    missing = [item for item in df]
-    return [miss for miss in missing if miss not in cols]
+    Missing_columns = [miss for miss in cols if miss not in  df.columns]
+    assert not Missing_columns, f"The missing columns is {Missing_columns}"
 
 def assert_non_empty(df, name="df"):
-    return
+    assert not df.empty, f"The Data ({name}) is an empty"
